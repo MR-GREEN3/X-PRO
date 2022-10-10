78b2cddb6a9af738b2f636df102fb0ffcb8d13ea
MR-GREEN <109001546+MR-GREEN3@users.noreply.github.com>
2022-10-10 18:11:19 +0600

    Add files via upload

--- X-PRO.py
+++ X-PRO.py
@@ -1,17 +1,3 @@
-import os,time, platform
-
-time.sleep(4)
-
-print("\x1b[38;5;46m CONGRATULATION YOUR DEVICE SUPPORT MY TOOLS") 
-
-time.sleep(4)
-print("\x1b[38;5;46m WELCOME TO MY TOOLS MR.GREEN ✓")
-
-
-
-
-
-
 
 import os, sys
 
